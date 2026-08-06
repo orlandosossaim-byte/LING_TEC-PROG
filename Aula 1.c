54084807d6af73bbcaba7805d99f2eb1a05bc472
Sossai <orlandosossaim@gmail.com>
2026-08-06 11:23:15 -0300

    Update Aula 1.c

--- Aula 1.c
+++ Aula 1.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
 	float a, b, c;
-	
+	//Aula feita no dia 06/08/26, o professor fez somente com o printf, o scanf eu adicionei!
 	printf("Digite o primeiro numero que quer Multiplicar: ");
 	scanf("%f", &a);
 	
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 	scanf("%f", &b);
 	
 	c = a * b;
-	printf("A Multiplicação de %f * %f = %f\n", a, b, c);
+	printf("A MultiplicaÃ§Ã£o de %f * %f = %f\n", a, b, c);
 	
 	return 0;
 }
