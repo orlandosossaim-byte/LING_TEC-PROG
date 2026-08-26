ea5d4eab4e7e412181b4b765c3e42a25785be621
Sossai <orlandosossaim@gmail.com>
2026-08-26 12:11:26 -0300

    Add files via upload

---
+++ aula6.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	/*
+	int a, b, c, r;
+	
+printf("Digite os tres numeros a serem comparados: ");
+scanf("%d %d %d", &a, &b, &c);
+	
+	if (a > b){
+		
+		r = a;
+		
+	}else{
+		r = b;	
+	}
+		
+	
+	
+	
+	
+	if (r < c){
+		
+		r = c;
+	}
+	
+	printf("%d eh maior", r );
+	
+	*/
+	
+
+    int num;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
+
+    if (num	%2 == 0) 
+        printf("O numero %d eh par", num); else printf("O numero %d eh impar", num);
+    
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
