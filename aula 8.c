7cda6bfa641d39c5c9f4c1f66ec1c7546d9b6341
Sossai <orlandosossaim@gmail.com>
2026-09-03 10:21:32 -0300

    02/09/2026

---
+++ aula 8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("\nExplicacao do trabalho, montagem de grupo e montagem de prototipo do trabalho");
+	return 0;
+}
